9ce17ecfbaa0af367d370c76a1a5ec98470fd113
Danil <danig1991@mail.ru>
2024-10-26 16:51:42 +0300

    ДЗ: Добавление книги

--- main.py
+++ main.py
@@ -4,45 +4,90 @@
 # которая выводит в консоль названия всех книг в библиотеке.
 # Если в библиотеке нет книг, функция выводит сообщение об этом.
 
+
+# Реализуйте функцию `add_book(title, author, year)`, которая добавляет книгу
+# в словарь `library`. Поле `наличие` при добавлении новой книги должно быть `None`
+# (означает, что книга в библиотеке, но не определен ее статус).
+# Если книга с таким названием уже существует,
+# программа должна предложить обновить информацию о ней.
+# Функция должна вывести сообщение об успешном добавлении/обновлении
+# информации о книге с ее названием
+
 library = {
-    # "Отцы и дети": {
-    #     "autor": "И. С. Тургенев",
-    #     "year": 1862,
-    #     "is_availability": True
-    # },
-    # "Мастер и Маргарита": {
-    #     "autor": "М. А. Булгаков",
-    #     "year": 1966,
-    #     "is_availability": True
-    # },
-    # "Евгений Онегин": {
-    #     "autor": "А. С. Пушкин",
-    #     "year": 1833,
-    #     "is_availability": True
-    # },
-    # "Преступление и наказание": {
-    #     "autor": "Ф. М. Достоевский",
-    #     "year": 1866,
-    #     "is_availability": True
-    # },
-    # "Мёртвые души": {
-    #     "autor": "Н. В. Гоголь",
-    #     "year": 1842,
-    #     "is_availability": True
-    # }
+    "Отцы и дети": {
+        "author": "И. С. Тургенев",
+        "year": 1862,
+        "is_availability": True
+    },
+    "Мастер и Маргарита": {
+        "author": "М. А. Булгаков",
+        "year": 1966,
+        "is_availability": True
+    },
+    "Евгений Онегин": {
+        "author": "А. С. Пушкин",
+        "year": 1833,
+        "is_availability": True
+    },
+    "Преступление и наказание": {
+        "author": "Ф. М. Достоевский",
+        "year": 1866,
+        "is_availability": True
+    },
+    "Мёртвые души": {
+        "author": "Н. В. Гоголь",
+        "year": 1842,
+        "is_availability": True
+    }
 }
 
 
-def book_list_view(dict_library):
-    list_availability = []
-    for availability in dict_library.values():
-        list_availability.append(availability.get("is_availability"))
-
-    if dict_library == {} or set(list_availability) == {False}:
-        print("В библиотеке нет книг")
+def book_list_view(library):
+    if not library:
+        print("В библиотеке нет книг.")
     else:
-        for book_name in dict_library.keys():
-            print(book_name)
+        for title in library.keys():
+            print(title)
 
 
-book_list_view(library)
+def add_book(title, author, year):
+    new_book = {
+        title: {
+            "author": author,
+            "year": year,
+            "is_availability": None
+        }
+    }
+    return library.update(new_book)
+
+
+def run():
+    while True:
+        try:
+            info = int(input("Введите число соответствующее действию:\n"
+                             "1 - Показать список книг\n"
+                             "2 - Добавить книгу\n"
+                             "0 - Завершить программу\n"))
+            if info == 1:
+                book_list_view(library)
+            elif info == 2:
+                title = input("Введите название книги:\n")
+
+                for book_name in library.keys():
+                    if book_name == title:
+                        print(f"\nКнига с названием \"{title}\" существует.\n"
+                              f"Обновите информацию о ней!\n")
+
+                author = input("Введите автора произведения:\n")
+                year = int(input("Введите год издания книги:\n"))
+                add_book(title, author, year)
+            elif info == 0:
+                break
+            else:
+                print("Соответствие не найдено!")
+
+        except ValueError:
+            print("Ввод некорректного значения")
+
+
+run()
